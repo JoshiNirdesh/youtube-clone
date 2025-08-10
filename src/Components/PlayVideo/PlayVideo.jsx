9248fa50bba56ef8f30c19f14277e31417e62007
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-08-10 06:54:31 +0545

    play video

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./PlayVideo.css";
-
+import video1 from "../../assets/video.mp4";
+import like from "../../assets/like.png";
+import dislike from "../../assets/dislike.png";
+import share from "../../assets/share.png";
+import save from "../../assets/save.png";
+import jack from "../../assets/jack.png";
+import user_profile from "../../assets/user_profile.jpg";
 const PlayVideo = () => {
-  return <div></div>;
+  return (
+    <div className="play-video">
+      <video src={video1} controls autoPlay muted></video>
+      <h3>Best Youtube Channel To Learn Web Development</h3>
+      <div className="play-video-info">
+        <p>1525 Views &bull; 2 days ago</p>
+        <div>
+          <span>
+            <img src={like} alt="" />
+            125
+          </span>
+          <span>
+            <img src={dislike} alt="" />2
+          </span>
+          <span>
+            <img src={share} alt="" />
+            Share
+          </span>
+          <span>
+            <img src={save} alt="" />
+            Save
+          </span>
+        </div>
+      </div>
+      <hr />
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>Greatstack</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>Channel that makes learning Easy</p>
+        <p>Subscribe GreatStack to Watch More Tutorials on web development</p>
+        <hr />
+        <h4>130 Comments</h4>
+      </div>
+    </div>
+  );
 };
 
 export default PlayVideo;
